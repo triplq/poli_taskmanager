c1de18d4dd54e71fba718962ef42ee85d5d47ab6
triplq <alexy.yaroshenko05@icloud.com>
2026-09-12 19:35:26 +0300

    Tasks complete and tested

--- poli_manager.cpp
+++ poli_manager.cpp
@@ -28,11 +28,11 @@ private:
 	std::time_t deadline;
 
 public:
-	DeadlineTask(const std::string& n_text, const std::time_t& n_deadline) : Task(n_text), deadline(n_deadline) { };
+	DeadlineTask(const std::string& n_text, time_t n_deadline) : Task(n_text), deadline(n_deadline) { };
 
 	DeadlineTask(const DeadlineTask& other) : Task(other.get_text()), deadline(other.deadline) { }
 
-	const std::time_t& get_deadline() const {
+	time_t get_deadline() const {
 		return deadline;
 	}
 
@@ -57,7 +57,7 @@ private:
 	std::time_t last_complete_day;
 
 public:
-	ReccuringTask(const std::string& n_text, const time_t& n_recure, const std::time_t& n_last_complete_day) : Task(n_text) {
+	ReccuringTask(const std::string& n_text, time_t n_recure, time_t n_last_complete_day) : Task(n_text) {
 		if(n_recure < 86400){
 			throw std::invalid_argument("Quantity of days can't be zero in reccuring task");
 		}
@@ -71,7 +71,7 @@ public:
 		return recure;
 	}
 
-	const std::time_t& get_last_complete_day() const {
+	time_t get_last_complete_day() const {
 		return last_complete_day;
 	}
 
@@ -93,16 +93,42 @@ public:
 
 	bool is_expired() const {
 		std::time_t now = std::time(nullptr);
-		return (now > last_complete_day + recure)
+
+		return (now > last_complete_day + recure);
 	}
 };
 
 int main() {
 	Task task("Work");
 
-	std::time_t deadline = std::time(nullptr) + (86400 * 5);
+	std::time_t deadline = std::time(nullptr) + (86400 * 5); //5 days
 	DeadlineTask task_with_deadline("Call",deadline);
 
+	std::time_t recure = 86400 * 7; //week
+	std::time_t last_complete_day = std::time(nullptr) - (86400 * 9);
+	ReccuringTask task_with_recure("Drink", recure, last_complete_day);
+
+	std::cout << "============TESTING TASK============\n";
+	std::cout << task.get_text() << ' ' << task.to_string() << '\n';
+
+	Task task2 = task;
+
+	std::cout << task2.get_text() << ' ' << task2.to_string() << '\n';
+
+	std::cout << "============TESTING DEADLINE============\n";
+
+	std::time_t local_deadline = task_with_deadline.get_deadline();
+	std::tm tm_deadline = *std::localtime(&local_deadline);
+	std::cout << std::asctime(&tm_deadline) << " Is it expired? " << task_with_deadline.is_expired() << ' ' << task_with_deadline.to_string() << '\n';
+
+	std::cout << "===============TESTING RECURE============\n";
+
+	time_t local_recure = task_with_recure.get_recure();
+	std::tm tm_recure = *std::localtime(&local_recure);
+	time_t local_lcd = task_with_recure.get_last_complete_day();
+	std::tm tm_lcd = *std::localtime(&local_lcd);
+
+	std::cout << asctime(&tm_recure) << task_with_recure.to_string() << " Is it expired? " << task_with_recure.is_expired() << ' ' << asctime(&tm_lcd);
 
 
 	return 0;
